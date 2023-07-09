32a046a3a4f43e531216e3bac610e6f75eb3174c
Caleb Crouch <cecrouch01@gmail.com>
2023-07-08 21:30:08 -0400

    addition of event listener to search bar

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var searchBtn = document.querySelector(".btn");
+
+
+function getCityWeather() {
+    
+
+}
+
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    getCityWeather();
+})
+
